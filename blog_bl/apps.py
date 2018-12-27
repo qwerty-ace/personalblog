038659d8bcefd6e397a0c8977573ff479fa668b8
personalblog <amina.shamil555@gmail.com>
2018-12-27 06:11:24 +0600

    first commit

---
+++ blog_bl/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BlogBlConfig(AppConfig):
+    name = 'blog_bl'
---
+++ blog_bl/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+class Post (models.Model):
+	author= models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+	title = models.CharField(max_length=200)
+	text=models.TextField()
+	created_date=models.DateTimeField(default=timezone.now)
+	published_date=models.DateTimeField(blank=True, null=True)
+
+	def publish(self):
+		self.published_date=timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.title
+
+
+
+# Post (models.Model)-so Django will know that we should save in db
+
+# charfie-limited Text-not limited
+# models.ForeignKey – this is a link to another model.1
+
+# ForeignKey-many to one relationship, settings.AUTH_USER_MODEL can have several post 
+
+# on_delete=models.CASCADE is an SQL standard.
+# CASCADE: When the referenced object is deleted, 
+# also delete the objects that have references to it (When you remove 
+# a blog post for instance, you might want to delete comments as well). SQL equivalent: CASCADE.
