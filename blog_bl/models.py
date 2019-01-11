a36a2ebed28cec2edfd10e3353e83d22f71264b1
personalblog <amina.shamil555@gmail.com>
2019-01-12 05:55:46 +0600

    no functionalities

--- blog_bl/models.py
+++ blog_bl/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 
 class Post (models.Model):
-	author= models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+	author= models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE, related_name='posts')
 	title = models.CharField(max_length=200)
 	text=models.TextField()
 	created_date=models.DateTimeField(default=timezone.now)
@@ -16,6 +16,9 @@ class Post (models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def snipped(self):
+		return self.body[0:50]+"..." 	
 
 
 
--- blog_bl/views.py
+++ blog_bl/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from django.utils import timezone
+from . import forms
+
 
 # Create your views here.
+def post_list(request):
+	posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog_bl/post_list.html', {'posts': posts})
+
+def post_create(request):
+	form=forms.CreatePost()
+	if request.method=='POST':
+		# form=forms.CreatePost(request.POST, request.FILES)
+		form=forms.CreatePost(request.POST)
+		if form.is_valid():
+			return redirect ('post_list')
+		else:
+			form=forms.CreatePost()
+	return render(request, 'blog_bl/post_create.html', {'form':form})	
